27a48fd8016bfceb18a5acff1eecf39da6ffc507
안수연 (Suyeon Ahn) <werther0704@gmail.com>
2025-12-15 23:58:15 +0900

    #18 [feat] 상품 목록 조회 API 구현

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductListSerializer
+
+@api_view(["GET"])
+def product_list(request):
+    qs = Product.objects.all()
+
+    # 1) 예금 / 적금 타입
+    product_type = request.GET.get("type")
+    if product_type:
+        qs = qs.filter(type=product_type)
+
+    # 2) 비대면 가입 여부
+    is_nftf = request.GET.get("is_non_face_to_face")
+    if is_nftf in ["true", "false"]:
+        qs = qs.filter(is_non_face_to_face=(is_nftf == "true"))
+
+    # 3) 보호 여부
+    is_dp = request.GET.get("is_deposit_protected")
+    if is_dp in ["true", "false"]:
+        qs = qs.filter(is_deposit_protected=(is_dp == "true"))
+
+    # 4) 최대 최소 
+    min_base_rate = request.GET.get("min_base_rate")
+    if min_base_rate:
+        try:
+            qs = qs.filter(base_rate__gte=float(min_base_rate))
+        except ValueError:
+            pass
+
+    min_max_rate = request.GET.get("min_max_rate")
+    if min_max_rate:
+        try:
+            qs = qs.filter(max_rate__gte=float(min_max_rate))
+        except ValueError:
+            pass
+
+    # 5) 분류
+    sort = request.GET.get("sort")
+    if sort == "max_rate_desc":
+        qs = qs.order_by("-max_rate", "-base_rate")
+    elif sort == "base_rate_desc":
+        qs = qs.order_by("-base_rate", "-max_rate")
+    else:
+        qs = qs.order_by("-max_rate", "-base_rate")
+
+    serializer = ProductListSerializer(qs, many=True)
+    return Response(serializer.data)
