d33039ffa004991eb586d5c222cc55cda83b7961
wave-rec <werther0704@gmail.com>
2025-12-17 23:52:02 +0900

    #24 [feat]: Product 찜 모델 추가

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Product(models.Model):
     fin_prdt_cd = models.CharField(max_length=50, unique=True)
@@ -38,3 +39,10 @@ class ProductRate(models.Model):
         return f"{self.product.name} - {self.save_terms_months}개월" 
 
 
+class Favorite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="favorites")
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="favorite_by")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "product")
