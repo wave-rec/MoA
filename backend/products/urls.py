e4624bc4e0fa5cd3f3e325881c7d552974201cb3
wave-rec <werther0704@gmail.com>
2025-12-18 19:44:05 +0900

    #29 [feat]: 찜 목록 조회 API 추가

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.product_list),
+    path("recommend/", views.product_recommend),
+    path("favorites/", views.favorite_list),
     path("<int:product_id>/", views.product_detail),
     path("<int:product_id>/favorite/", views.toggle_favorite),
-    path("recommend/", views.product_recommend),
 ]
