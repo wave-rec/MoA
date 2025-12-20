1f82fe9f53db548af5b41000544e6187ec5c7997
wave-rec <werther0704@gmail.com>
2025-12-21 01:14:33 +0900

    #51 [feat]: 모든 페이지에 탑버튼 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <RouterView />
+  <div class="app-container">
+    <RouterView />
+
+    <ScrollToTop />
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import ScrollToTop from '@/components/common/ScrollToTop.vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+.app-container {
+  min-height: 100vh;
+  position: relative;
+}
+</style>
